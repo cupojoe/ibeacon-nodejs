ddbe0b73793c76e594ba3360905cfce573e1e752
cupojoe <joe.fraga@gmail.com>
2014-06-09 15:58:40 -0400

    Updated practice's names

--- routes/practices/get.js
+++ routes/practices/get.js
@@ -3,10 +3,13 @@ module.exports = function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
   res.send([
-    {name: "FED", color: "FFA500"},
-    {name: "Design", color: "CDCD00"},
-    {name: "ID", color: "C1FFC1"},
-    {name: "Strategy", color: "00B2EE"}
+    {name: 'Account Management', color: '3D59AB'},
+    {name: 'Project Management', color: 'DC143C'},
+    {name: 'Experience Strategy', color: 'FF6347'},
+    {name: 'Interaction Design', color: 'C1FFC1'},
+    {name: 'Visual Design', color: 'CDCD00'},
+    {name: 'Front-End Development', color: 'FFA500'},
+    {name: 'Content', color: '00B2EE'}
   ]);
   next();
 }
