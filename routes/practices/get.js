5c4b00d559c9586d55ab828a54f375836d05cc1f
cupojoe <joe.fraga@gmail.com>
2014-06-11 12:16:04 -0400

    Updated practice colors

--- routes/practices/get.js
+++ routes/practices/get.js
@@ -3,13 +3,13 @@ module.exports = function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
   res.send([
-    {name: 'Account Management', color: '3D59AB'},
-    {name: 'Project Management', color: 'DC143C'},
-    {name: 'Experience Strategy', color: 'FF6347'},
-    {name: 'Interaction Design', color: 'C1FFC1'},
-    {name: 'Visual Design', color: 'CDCD00'},
-    {name: 'Front-End Development', color: 'FFA500'},
-    {name: 'Content', color: '00B2EE'}
+    {name: 'Account Management', color: 'E74C3C'},
+    {name: 'Project Management', color: 'EA772A'},
+    {name: 'Experience Strategy', color: 'F1C40F'},
+    {name: 'Interaction Design', color: 'AD51AD'},
+    {name: 'Visual Design', color: '0098CC'},
+    {name: 'Front-End Development', color: '2BB275'},
+    {name: 'Content', color: 'A6BEBF'}
   ]);
   next();
 }
